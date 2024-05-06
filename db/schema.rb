14f2919f8549ef82db9c11755d4dd8ab4b28305a
brandaoplaster <brandaoplaster@gmail.com>
2024-05-05 23:17:08 -0300

    feat: Add references category in task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_05_022753) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_06_021451) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_05_022753) do
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
+  add_foreign_key "tasks", "categories"
 end

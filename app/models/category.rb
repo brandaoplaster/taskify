31eaff2c00d470e95d89351ea0fc5c1470964c9c
brandaoplaster <brandaoplaster@gmail.com>
2024-05-05 23:17:40 -0300

    feat: Add relationship category in task

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
 class Category < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end

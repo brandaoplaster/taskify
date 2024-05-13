15cc59a46ff1954cfab3cae2f42a26f379dff919
brandaoplaster <brandaoplaster@gmail.com>
2024-05-12 22:47:47 -0300

    feat: Adding relationship and validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  has_many :tasks, dependent: :destroy
 end

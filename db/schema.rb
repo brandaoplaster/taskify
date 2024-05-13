054cbdbe94e7e3ba4fc626f76c0e46a7f4792078
brandaoplaster <brandaoplaster@gmail.com>
2024-05-12 23:03:34 -0300

    feat: Adding user references to migrate tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_012530) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_020137) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,7 +24,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_012530) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_012530) do
   end
 
   add_foreign_key "tasks", "categories"
+  add_foreign_key "tasks", "users"
 end
